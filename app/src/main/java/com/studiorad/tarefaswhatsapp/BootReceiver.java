a35f15b74d111f86e4f9f5cf82c65dc509bc1f2a
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-06-28 19:43:47 -0300

    Reagendar notificacao apos reiniciar celular

---
+++ app/src/main/java/com/studiorad/tarefaswhatsapp/BootReceiver.java
@@ -0,0 +1,15 @@
+package com.studiorad.tarefaswhatsapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            NotificationScheduler.ensureChannel(context);
+            NotificationScheduler.scheduleDaily(context, NotificationScheduler.getTime(context));
+        }
+    }
+}
